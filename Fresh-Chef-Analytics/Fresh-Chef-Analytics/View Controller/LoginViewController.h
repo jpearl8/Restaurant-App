c5ad3ca115944c286941bb8f1fecb4cb558d2a63
Robert Gallardo <rgallardo@fb.com>
2019-07-16 13:58:14 -0700

    add login and sign up view controllers with commented out code

---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/LoginViewController.h
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.h
+//  Fresh-Chef-Analytics
+//
+//  Created by rgallardo on 7/16/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import "ViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LoginViewController : ViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/LoginViewController.m
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.m
+//  Fresh-Chef-Analytics
+//
+//  Created by rgallardo on 7/16/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import "Parse/Parse.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+//    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+//
+//- (void)loginUser {
+//    NSString *username = self.usernameField.text;
+//    NSString *password = self.passwordField.text;
+//    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+//        if (error != nil) {
+//            // if password is for waiter go to waiter view
+////            [PFUser logInWithUsername:username waiterPassword:
+//            NSLog(@"User log in failed: %@", error.localizedDescription);
+//        } else {
+//            NSLog(@"User logged in successfully");
+//            // if password is for manager then go to profile view
+//            // if password is for waiter go to waiter view
+//
+//            // display view controller that needs to shown after successful login
+//            [self performSegueWithIdentifier:@"logInSegue" sender:nil];
+//        }
+//    }];
+//}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
