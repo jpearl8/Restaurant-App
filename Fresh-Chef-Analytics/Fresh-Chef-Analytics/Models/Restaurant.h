f99d5a874fea34c2e82c3eea9daaf01502fc9c13
Samantha (sam) Silverstein <selinons@fb.com>
2019-07-16 16:06:02 -0700

    adding dish model

---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/Models/Restaurant.h
@@ -0,0 +1,17 @@
+//
+//  Restaurant.h
+//  Fresh-Chef-Analytics
+//
+//  Created by selinons on 7/16/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Restaurant : PFObject
+
+@end
+
+NS_ASSUME_NONNULL_END
