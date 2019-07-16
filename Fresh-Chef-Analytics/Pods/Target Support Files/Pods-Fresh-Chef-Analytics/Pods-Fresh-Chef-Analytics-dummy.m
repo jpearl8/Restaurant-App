16ed37003ca1c2b788b723868a88a1ee1d6f6f21
Julia Pearl <jpearl@fb.com>
2019-07-16 10:55:55 -0700

    pods added

---
+++ Fresh-Chef-Analytics/Pods/Target Support Files/Pods-Fresh-Chef-Analytics/Pods-Fresh-Chef-Analytics-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fresh_Chef_Analytics : NSObject
+@end
+@implementation PodsDummy_Pods_Fresh_Chef_Analytics
+@end
---
+++ Fresh-Chef-Analytics/Pods/Target Support Files/Pods-Fresh-Chef-AnalyticsTests/Pods-Fresh-Chef-AnalyticsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fresh_Chef_AnalyticsTests : NSObject
+@end
+@implementation PodsDummy_Pods_Fresh_Chef_AnalyticsTests
+@end
