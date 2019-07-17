eaeb6e0aef44072c3e21a2f9f731d088c22353af
Julia Pearl <jpearl@fb.com>
2019-07-17 11:35:09 -0700

    Waiter view added, need restaurant model

---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Waiter VCs/WaitTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  WaitTableViewCell.m
+//  Fresh-Chef-Analytics
+//
+//  Created by jpearl on 7/17/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import "WaitTableViewCell.h"
+
+
+@implementation WaitTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+    self.checkBox.boxType = BEMBoxTypeSquare;
+    // Configure the view for the selected state
+}
+
+@end
--- Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Waiter VCs/WaiterViewController.m
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Waiter VCs/WaiterViewController.m
@@ -5,27 +5,58 @@
 //  Created by jpearl on 7/16/19.
 //
 
+
+/* TODO:
+get dish items from restaurant table
+hookup search bar
+pass final array on submit button of data table
+ */
 #import "WaiterViewController.h"
+#import "Dish.h"
+#import "WaitTableViewCell.h"
 
-@interface WaiterViewController ()
-
+@interface WaiterViewController () <UITableViewDelegate, UITableViewDataSource>
+    @property (weak, nonatomic) IBOutlet UITableView *menuItems;
+    @property (strong, nonatomic) NSArray <Dish *>*dishes;
 @end
 
 @implementation WaiterViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    self.menuItems.delegate = self;
+    self.menuItems.dataSource = self;
     // Do any additional setup after loading the view.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.dishes.count;
 }
-*/
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    WaitTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"Orders"];
+    Dish *dish = self.dishes[indexPath.row];
+    cell.dish = dish;
+    cell.name.text = dish.name;
+    cell.type.text = dish.type;
+    cell.description.text = dish.description;
+    PFFileObject *dishImageFile = (PFFileObject *)dish.image;
+    [dishImageFile getDataInBackgroundWithBlock:^(NSData *imageData, NSError *error) {
+        if(!error){
+            cell.image.image = [UIImage imageWithData:imageData];
+        }
+    }];
+    return cell;
+}
+
+
+//#pragma mark - Navigation
+//
+////// In a storyboard-based application, you will often want to do a little preparation before navigation
+////- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+////    // Get the new view controller using [segue destinationViewController].
+////    // Pass the selected object to the new view controller.
+////}
+////*/
 
 @end
