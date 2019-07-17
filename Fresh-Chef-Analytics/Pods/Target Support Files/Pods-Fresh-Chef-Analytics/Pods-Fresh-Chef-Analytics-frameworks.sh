71a53b5ff4514daceec2f7978bad70d19c087fea
Samantha (sam) Silverstein <selinons@fb.com>
2019-07-17 11:13:46 -0700

    pods for drop down menu and textview + placeholder

--- Fresh-Chef-Analytics/Pods/Target Support Files/Pods-Fresh-Chef-Analytics/Pods-Fresh-Chef-Analytics-frameworks.sh
+++ Fresh-Chef-Analytics/Pods/Target Support Files/Pods-Fresh-Chef-Analytics/Pods-Fresh-Chef-Analytics-frameworks.sh
@@ -162,11 +162,15 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MKDropdownMenu/MKDropdownMenu.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/UITextView+Placeholder/UITextView_Placeholder.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Bolts/Bolts.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MKDropdownMenu/MKDropdownMenu.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Parse/Parse.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/UITextView+Placeholder/UITextView_Placeholder.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
