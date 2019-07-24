61deb5cb4dd5ec143a92d132ca5d36fb8cef218c
Robert Gallardo <rgallardo@fb.com>
2019-07-24 15:19:25 -0700

    move menu listed to menu data tab and add table view to hold data visualization. Make empty view and view controller for order list in manager view

---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Manager VCs/DataViewTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewTableViewController.h
+//  Fresh-Chef-Analytics
+//
+//  Created by rgallardo on 7/24/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataViewTableViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Manager VCs/DataViewTableViewController.m
@@ -0,0 +1,131 @@
+//
+//  DataViewTableViewController.m
+//  Fresh-Chef-Analytics
+//
+//  Created by rgallardo on 7/24/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import "DataViewTableViewController.h"
+
+@interface DataViewTableViewController ()
+
+@end
+
+@implementation DataViewTableViewController {
+    BOOL isCellExpanded;
+    NSIndexPath *selectedIndexPath;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    isCellExpanded = NO;
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 6;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    selectedIndexPath = indexPath;
+    if(isCellExpanded){
+        isCellExpanded = NO;
+    } else {
+        isCellExpanded = YES;
+    }
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if([indexPath compare:selectedIndexPath] == NSOrderedSame) {
+        if(isCellExpanded == YES){
+            return self.view.frame.size.height * 0.75;
+        } else {
+            return 75;
+        }
+    } else {
+        return 75;
+    }
+//    if(indexPath.row == 0){
+//        //return normal height because not expandable
+//    } else if (indexPath.row == 1){
+//        //if not selected make it size of screen
+//
+//    } else {
+//        if(isCellExpanded == YES){
+//            return 75;
+//        } else {
+//            return 500;
+//        }
+//    }
+//    return 75;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
