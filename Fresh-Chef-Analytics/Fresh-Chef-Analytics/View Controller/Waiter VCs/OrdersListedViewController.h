dba5279ade645acfcfca890c96e6fc5fcd835234
Robert Gallardo <rgallardo@fb.com>
2019-07-24 11:44:07 -0700

    add static table view to menu data

---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Waiter VCs/OrdersListedViewController.h
@@ -0,0 +1,17 @@
+//
+//  OrdersListedViewController.h
+//  Fresh-Chef-Analytics
+//
+//  Created by rgallardo on 7/24/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface OrdersListedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
