b61c988c8c750b6f273c75d8d2f6c14a9af3bdea
Samantha (sam) Silverstein <selinons@fb.com>
2019-07-26 15:14:34 -0700

    setting up manager view of order list

--- Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Manager VCs/OrdersManagerViewController.h
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Manager VCs/OrdersManagerViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface OrdersManagerViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
+@interface OrdersManagerViewController : UIViewController
 
 @end
 
