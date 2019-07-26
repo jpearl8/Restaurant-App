20c04c923d0d6682e1d6770b4b9d4e15d93e1fe2
Samantha (sam) Silverstein <selinons@fb.com>
2019-07-26 10:50:22 -0700

    open orders by table dict

--- Fresh-Chef-Analytics/Fresh-Chef-Analytics/Models/OrderManager.m
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/Models/OrderManager.m
@@ -27,10 +27,34 @@
     [openOrderQuery findObjectsInBackgroundWithBlock:^(NSArray * openOrders, NSError * error) {
         if (!error)
         {
-            self.openOrders = openOrders;
-            fetchedOpenOrders(self.openOrders, nil);
+            self.allOpenOrders = openOrders;
+            [self sortOrdersByTable];
+            fetchedOpenOrders(self.allOpenOrders, nil);
         }
     }];
+}
+- (void) sortOrdersByTable
+{
+    self.openOrdersByTable = [[NSMutableDictionary alloc] init];
+    NSArray *ordersByTable;
+    for (OpenOrder *order in self.allOpenOrders)
+    {
+        [self addOrderToDict:order toArray:ordersByTable];
+    }
+}
+- (void) addOrderToDict : (OpenOrder *) order toArray : (NSArray *) ordersOfTable
+{
+    NSString *table = [order.table stringValue];
+    if (self.openOrdersByTable[table] != nil)
+    {
+        ordersOfTable = self.openOrdersByTable[table];
+        ordersOfTable = [ordersOfTable arrayByAddingObject:order];
+    }
+    else
+    {
+        ordersOfTable = [NSArray arrayWithObject:order];
+    }
+    [self.openOrdersByTable setObject:ordersOfTable forKey:table];
 }
 - (void) fetchClosedOrderItems:(PFUser *) restaurant  withCompletion:(void (^)(NSArray * closedOrders, NSError * error))fetchedClosedOrders
 {
@@ -45,30 +69,31 @@
         }
     }];
 }
-- (void) fetchOrdersToClose : (PFUser * ) restaurant withTable : (NSNumber *) table forWaiter : (Waiter *) waiter withCompletion : (void (^)(NSArray *orders, NSError * error))completion
-{
 
-    PFQuery *orderQuery;
-    orderQuery = [OpenOrder query];
-    [orderQuery whereKey:@"restaurantId" equalTo:restaurant.objectId];
-    //[orderQuery includeKey:@"dish"];
-    //    [orderQuery whereKey:@"table" equalTo:table];
-    [orderQuery findObjectsInBackgroundWithBlock:^(NSArray *_Nullable orders, NSError * _Nullable error) {
-        if (orders)
-        {
-            self.ordersToDelete = orders;
-            
-            completion(orders, nil);
-            
-        }
-        else
-        {
-            NSLog(@"Error: %@", error.localizedDescription);
-            completion(nil, error);
-        }
-    }];
-    
-}
+//- (void) fetchOrdersToClose : (PFUser * ) restaurant withTable : (NSNumber *) table forWaiter : (Waiter *) waiter withCompletion : (void (^)(NSArray *orders, NSError * error))completion
+//{
+//
+//    PFQuery *orderQuery;
+//    orderQuery = [OpenOrder query];
+//    [orderQuery whereKey:@"restaurantId" equalTo:restaurant.objectId];
+//    //[orderQuery includeKey:@"dish"];
+//    //    [orderQuery whereKey:@"table" equalTo:table];
+//    [orderQuery findObjectsInBackgroundWithBlock:^(NSArray *_Nullable orders, NSError * _Nullable error) {
+//        if (orders)
+//        {
+//            self.ordersToDelete = orders;
+//
+//            completion(orders, nil);
+//
+//        }
+//        else
+//        {
+//            NSLog(@"Error: %@", error.localizedDescription);
+//            completion(nil, error);
+//        }
+//    }];
+//
+//}
 - (void) fetchDishInOrdersToClose : (OpenOrder *) temp withCompletion : (void (^)(NSArray * dishes, NSError * error))completion
 {
     PFQuery *dishQuery;
@@ -88,10 +113,10 @@
     completion(nil, nil);
 
 }
-- (void) deletingOrders : (NSArray *) orders withRestaurant : (PFUser * ) restaurant withTable : (NSNumber *) table forWaiter : (Waiter *) waiter withCustomerNum : (NSNumber *) customerNum withCompletion : (void (^)(NSError * error))completion
+- (void) deletingOrderswithTable : (NSNumber *) table forWaiter : (Waiter *) waiter withCustomerNum : (NSNumber *) customerNum withCompletion : (void (^)(NSError * error))completion
 {
     ClosedOrder *newAddition = [ClosedOrder new];
-    newAddition.restaurant = restaurant;
+    newAddition.restaurant = PFUser.currentUser;
     newAddition.table = table;
     newAddition.numCustomers = customerNum;
 
@@ -99,31 +124,32 @@
     __block NSString *dishName;
     __block NSNumber *dishAmount;
 
-    for (int i = 0; i < orders.count; i++)
+    NSArray *ordersToClose = self.openOrdersByTable[[table stringValue]];
+    for (int i = 0; i < [ordersToClose count]; i++)
     {
-        OpenOrder *temp = orders[i];
+        OpenOrder *temp = ordersToClose[i];
         [self fetchDishInOrdersToClose:temp withCompletion:^(NSArray *dishes, NSError *error) {
             if (dishes)
             {
-                
                 Dish * tempDish = dishes[0];
                 dishName = tempDish.name;
                 dishAmount = temp.amount;
-                [orders[i] deleteInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
+                [ordersToClose[i] deleteInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
                     if (succeeded)
                     {
                         NSLog(@"Order removed");
                         [newAddition addObject:dishName forKey:@"dishes"];
                         [newAddition addObject:dishAmount forKey:@"amounts"];
                         doneWithArray++;
-                        if (doneWithArray >= [orders count])
+                        if (doneWithArray >= [ordersToClose count])
                         {
                             [newAddition saveInBackgroundWithBlock:^(BOOL succeeded, NSError * _Nullable error) {
                                 NSLog(@"executing something?");
                                 if (!error)
                                 {
                                     NSLog(@"Closed order saved");
-                                    [self fetchOpenOrderItems:restaurant withCompletion:^(NSArray * _Nonnull openOrders, NSError * _Nonnull error) {
+                                    [self fetchOpenOrderItems:PFUser.currentUser
+                                               withCompletion:^(NSArray * _Nonnull openOrders, NSError * _Nonnull error) {
                                         if (succeeded)
                                         {
                                             NSLog(@"Updated open orders");
