606acc6f792dd05f9a4031cc1d23695233fd44b6
Samantha (sam) Silverstein <selinons@fb.com>
2019-07-29 23:15:32 -0700

    cleaned up scatter plot, radar chart in progress

---
+++ Fresh-Chef-Analytics/Fresh-Chef-Analytics/View Controller/Manager VCs/Child VCs/BarChartViewController.h
@@ -0,0 +1,17 @@
+//
+//  BarChartViewController.h
+//  Fresh-Chef-Analytics
+//
+//  Created by selinons on 7/29/19.
+//  Copyright © 2019 julia@ipearl.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BarChartViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
